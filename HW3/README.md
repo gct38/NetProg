f33318cfb22fc9a7e9666ecaad6d56db1802794e
Saptarshi Ganguly <gangus@rpi.edu>
2020-11-02 16:03:03 -0500

    LiveShare Session 1

--- HW3/README.md
+++ HW3/README.md
@@ -1,5 +1,5 @@
 # README
 
-## Run this to recompile the proto
+### Run this to compile the protos
 
-      python3 -m grpc_tools.protoc -I=HW3 --python_out=HW3 --grpc_python_out=HW3 HW3/csci4220_hw3.proto
+      python3 run_codegen.py
--- HW3/hw3.py
+++ HW3/hw3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from csci4220_hw3_pb2_grpc import KadImplServicer
 from concurrent import futures
 import sys  # For sys.argv, sys.exit()
 import socket  # for gethostbyname()
@@ -8,6 +9,18 @@ import grpc
 
 import csci4220_hw3_pb2
 import csci4220_hw3_pb2_grpc
+
+class KadImplServicer(csci4220_hw3_pb2_grpc.KadImplServicer):
+	def __init__(self):
+		pass
+
+	def FindNode(self, IDKey, context):
+		pass
+
+	def FindValue(self, request, context):
+		pass
+	def Quit(self, request, context):
+		pass
 
 def run():
 	if len(sys.argv) != 4:
@@ -19,6 +32,13 @@ def run():
 	k = int(sys.argv[3])
 	my_hostname = socket.gethostname() # Gets my host name
 	my_address = socket.gethostbyname(my_hostname) # Gets my IP address from my hostname
+   
+	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+	csci4220_hw3_pb2_grpc.add_KadImplServicer_to_server(
+		KadImplServicer(), server)
+	server.add_insecure_port('[::]:' + my_port)
+	server.start()
+	server.wait_for_termination()
 
 	''' Use the following code to convert a hostname to an IP and start a channel
 	Note that every stub needs a channel attached to it
@@ -30,5 +50,6 @@ def run():
 	#remote_port = int(remote_port_string)
 	#channel = grpc.insecure_channel(remote_addr + ':' + str(remote_port))
 
-if __name__ == '__main__':
-	run()
+	if __name__ == '__main__':
+		run()
+		
