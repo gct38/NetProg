5e84a427406a6f1f6c113763b186fe2925bc3c30
Gordon Tsang <gordontsang1@gmail.com>
2020-11-03 19:28:53 -0500

    Partially implemented Store RPC and implemented a function to update kbuckets within a node.

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -10,22 +10,25 @@ import grpc
 import csci4220_hw3_pb2
 import csci4220_hw3_pb2_grpc
 
-#TODO: implement container for k-buckets
+
 
 #Server Implementation
 class KadImplServicer(csci4220_hw3_pb2_grpc.KadImplServicer):
-	def __init__(self, local_id, port, k):
-		self.id = local_id
-		self.port = port
-		self.k = k
-		self.nodes = dict() #dict of all nodes key=id, val=Node
+	def __init__(self, local_id, port, address, k):
+		self.id = int(local_id)
+		self.port = int(port)
+		self.address = str(address)
+		self.k = int(k)
+		self.bucket = [[]]*4 	#initializing kbucket to list of 0's with length k
+		self.hashtable = dict() #key=key from KeyValue, value=KeyValue 
 
 	#return NodeList of k closest nodes to given ID
 	def FindNode(self, IDKey, context):
 		idkey = IDKey.idkey
 		distances = dict() #key=id, value=distance
 		responder = None
-		for id in self.nodes:
+		'''
+		for id in self.bucket:
 			if id==idkey:
 				responder = self.nodes[id]
 				continue
@@ -43,7 +46,7 @@ class KadImplServicer(csci4220_hw3_pb2_grpc.KadImplServicer):
 						to_remove = key 
 						break
 				del distances[to_remove]
-
+		'''
 		#creating NodeList from distances dict
 		#TODO: implement NodeList, don't think nodes parameter is right
 		return csci4220_hw3_pb2.NodeList(responding_node=responder, nodes=list(distances.values()))
@@ -53,30 +56,38 @@ class KadImplServicer(csci4220_hw3_pb2_grpc.KadImplServicer):
 	def FindValue(self, request, context):
 		pass
 
-	#Stores KeyValue at node w/ ID closest to Key
+	#Stores KeyValue at current node and returns IDKey
 	def Store(self, KeyValue, context):
-		difference = float('inf')
-		insert = None
-		for node in self.nodes:
-			if abs(node - KeyValue.key) < difference:
-				difference = abs(node - KeyValue.key)
-				insert = node 
-		#TODO: code to insert KeyValue into kbucket
-
-		
+		if KeyValue.key not in self.hashtable: 		
+			self.hashtable[KeyValue.key] = KeyValue
+			print("Storing key {:} at value {:}".format(KeyValue.key, KeyValue.value))
+		return csci4220_hw3_pb2.IDKey(node=csci4220_hw3_pb2.Node(id=self.id, 
+																 port=self.port, 
+																 address=self.address), 
+									  idkey=KeyValue.key)
 
 	#quit current node, removes all nodes from current node's kbucket
 	def Quit(self, request, context):
 		pass
 
-
-
 	#finds distance using XOR between 2 IDs
 	def __distance(self, id1, id2=self.id):
-		return id1 ^ id2
+		return id1 ^ id2	
 
-
-	
+	#updating kbuckets so that requester's id is most recent 
+	def __update_kbuckets(self, id):
+		found = False
+		for i in range(len(self.bucket)):
+			for j in range(len(self.bucket[i])):
+				if self.bucket[i][j].value == id:
+					index = j
+					found = True 
+					break
+			if found:
+				if index+1 != len(self.bucket[i])-1:
+					self.bucket[i] = self.bucket[i][:index] + self.bucket[i][index+1:] + [self.bucket[i][index]] 
+				else:
+					self.bucket[i] = self.bucket[i][:index] + [self.bucket[i][index]]
 
 
 #Client Implementation
@@ -96,15 +107,19 @@ def find_value(stub, key):
 	pass
 
 #store key:value as KeyValue
-#	stores to closest node id by key
+#	finds and stores to closest node id by key
 def store(stub, key, value):
+	#Determine where to store key-value pair
+
+	#Print record of storage at location
+	print('Storing key {:} at node {:}'.format(key, value))
+	#If the key is same as this node Store here
+	#Else send key-value pair to appropriate Node
 	pass
 
 #quit current node, removes all nodes from current node's kbucket
-def quit(stub):
-	pass
-
-
+def Quit(stub):
+	
 
 def run():
 	if len(sys.argv) != 4:
