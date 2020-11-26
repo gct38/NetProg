e87836d2e8f4202c3e1a9386d1d624452cfb0541
Gordon Tsang <gordontsang1@gmail.com>
2020-11-26 18:41:17 -0500

    Implementation of input argument parsing using SELECT for both STDIN and client/server.

--- HW4/hw4_client.py
+++ HW4/hw4_client.py
@@ -4,6 +4,10 @@ import select
 
 #TODO: add in logic for handling messages from Control Server
 
+#corresponds with QUIT command
+def quit(server):
+    server.close()
+
 def run_client():
     if len(sys.argv) != 7:
         print(f"Proper usage is {sys.argv[0]} [control address] [control port] [sensor id] [sensor range] [initial x] [initial y]")
@@ -14,25 +18,36 @@ def run_client():
     server_socket.connect((sys.argv[1], int(sys.argv[2])))
     server_socket.sendall(str(sys.argv).encode('utf-8'))
     while True:
-        #Sensor Client STDIN commands
-        inp = input().split()
-        if inp[0].upper() == "MOVE" and len(inp) == 3:
-            #send UPDATEPOSITION to control server
-            pass
-        elif inp[0].upper() == "SENDDATA" and len(inp) == 2:
-            pass
-        elif inp[0].upper() == "WHERE" and len(inp) == 2:
-            pass
-        elif inp[0].upper() == "UPDATEPOSITION" and len(inp) == 5:
-            pass
-        elif inp[0].upper() == "QUIT" and len(inp) == 1:
-            break
-        else:
-            print("Invalid arguments")
+        reads, writes, excepts = select.select([sys.stdin, server_socket],[],[])
+        for item in reads:
+            if item is server_socket:           #Receive message from Control Server
+                message = server_socket.recv(1024)
+                if message:
+                    message = str(message.decode('utf-8')).split()
+                    print("From Server:", message)
+                    if len(message) == 2 and message[0].upper() == "DATAMESSAGE":
+                        pass
+                    else:
+                        print("Invalid arguments from server")
+            elif item == sys.stdin:             #Sensor Client STDIN commands
+                stdin = sys.stdin.readline().strip()
+                message = stdin.split()
+                print("STDIN:", message)
+                if len(message) == 3 and message[0].upper() == "MOVE":
+                    #send UPDATEPOSITION to control server
+                    pass
+                elif len(message) == 2 and message[0].upper() == "SENDDATA":
+                    pass
+                elif len(message) == 2 and message[0].upper() == "WHERE":
+                    #server_socket.sendall(str(stdin).encode('utf-8'))
+                    pass
+                elif len(message) == 1 and message[0].upper() == "QUIT":
+                    quit(server_socket)
+                    return
+                else:
+                    print("Invalid arguments")
 
 
-    # Disconnect from the server
-    server_socket.close()
 
 if __name__ == '__main__':
     run_client()
--- HW4/hw4_control.py
+++ HW4/hw4_control.py
@@ -4,7 +4,6 @@ import select
 from math import sqrt
 
 
-#includes all nodes (sensors and base stations)
 class Sensor:
     def __init__(self, id, range, x, y):
         self.id = str(id)
@@ -14,11 +13,8 @@ class Sensor:
         self.num_links = 0
         self.connections = {}
 
-    def distance_to(self, other):
-        return sqrt((self.x-other.x)**2 + (self.y-other.y)**2)
-
     def in_range(self, other):
-        return self.distance_to(other) <= self._range
+        return distance_to(self,other) <= self._range
 
     def __str__(self):
         return "Sensor id: {}, x: {}, y: {}, connected to:{}".format(self.id, self.x, self.y, self.connections)
@@ -37,9 +33,6 @@ class BaseStation:
         self.num_links = int(num_links)
         self.connections = populate_connections(list_of_links)    #dict of anything the base is connected to (bases/clients)
 
-    def distance_to(self, other):
-        return sqrt((self.x-other.x)**2 + (self.y-other.y)**2)
-
     def __str__(self):
         return "Base Station id: {}, x: {}, y: {}, connected to: {}".format(self.id, self.x, self.y, str(self.connections))
 
@@ -49,6 +42,10 @@ class BaseStation:
     def __ne__(self, other):
         return not self.__eq__(other)
 
+#helper function to calculate distance between two objects (Sensor/BaseStation)
+def distance_to(obj, other):
+    return sqrt((obj.x-other.x)**2 + (obj.y-other.y)**2)
+
 #initializes all distances to -1 (used to initialize all the Sensor neighbors and their distances to -1)
 def populate_connections(links):
     connections = {}
@@ -56,6 +53,13 @@ def populate_connections(links):
         connections[item] = -1
     return connections
 
+#clean up disconnected clients as connections
+def remove_disconnected(nodes):
+    for id in nodes:
+        temp = nodes[id].connections.copy()
+        for conn_id in temp:
+            if conn_id not in nodes:
+                del nodes[id].connections[conn_id]
 
 #updates neighbors of all nodes
 def update_neighbors(nodes):
@@ -63,14 +67,14 @@ def update_neighbors(nodes):
         if type(nodes[id1]) == Sensor:
             for id2 in nodes:
                 if nodes[id1] != nodes[id2] and nodes[id1].in_range(nodes[id2]):
-                    nodes[id1].connections[id2] = nodes[id1].distance_to(nodes[id2])
-                    nodes[id2].connections[id1] = nodes[id2].distance_to(nodes[id1])
+                    nodes[id1].connections[id2] = distance_to(nodes[id1], nodes[id2])
+                    nodes[id2].connections[id1] = distance_to(nodes[id2], nodes[id1])
 
 #updates distances of all nodes
 def update_distance(nodes):
     for id in nodes:
         for connected_node in nodes[id].connections:
-            nodes[id].connections[connected_node] = nodes[id].distance_to(nodes[connected_node])
+            nodes[id].connections[connected_node] = distance_to(nodes[id], nodes[connected_node])
 
 #parse the base station txt file and returns dict of BaseStation objects (key = id of BaseStation)
 def parse_bases(file):
@@ -84,10 +88,12 @@ def parse_bases(file):
 def send_data():
     pass
 
-def quit(client_socket):
-    # Close the connection
-    client_socket.close()
-    print('clientsocket should be closed by now')
+#TODO: double check to make sure this is right
+#corresponds to quit command, closes all clients and then server
+def quit(inputs):
+    for socket in inputs:
+        #print('socket {} should be closed by now'.format(socket))
+        socket.close()
 
 
 def run_control():
@@ -95,65 +101,94 @@ def run_control():
         print(f"Proper usage is {sys.argv[0]} [port number] [base station file]")
         sys.exit(0)
 
+    inputs = [sys.stdin]
+    clients = {}                #stores all of our Sensor Clients
+    node_addresses = {}         #stores client address as key and id as value
     port = int(sys.argv[1])
     file = str(sys.argv[2])
     nodes = parse_bases(file)   #dict of all clients/base stations
     update_distance(nodes)
 
+    '''
     for id in nodes:
         print(nodes[id])
     print()
+    '''
 
     # Create a TCP socket
     listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listening_socket.bind(('', port))
     listening_socket.listen(5)
+    inputs.append(listening_socket)
 
     # Server loop
     while True:
-        (client_socket, address) = listening_socket.accept()
+        reads, writes, excepts = select.select(inputs,[],[])
+        for client in reads:
+            if client is listening_socket:      #Accepting new Sensor Client connections
+                (client_socket, address) = listening_socket.accept()
+                inputs.append(client_socket)
+            elif client == sys.stdin:           #Control Server STDIN commands
+                message = sys.stdin.readline().strip().split()
+                print("std in message: {}".format(message))
+                if len(message) == 3 and message[0].upper() == "SENDDATA":
+                    pass
+                    #TODO: implement send_data function
+                elif len(message) == 1 and message[0].upper() == "QUIT":
+                    quit(inputs)
+                    return
+                else:
+                    print("Invalid arguments")
+            else:                               #Receive message from Sensor Client
+                message = client.recv(1024)
+                if message:
+                    message = str(message.decode('utf-8'))
+                    #print("Client{} {}".format(client.getpeername(), client))
+                    print("From Client:", message)
+                    if message.find('hw4_client.py') != -1:
+                        message = message.replace('[','').replace(']','').replace('\'','').split(',')
+                        nodes[str(message[3]).strip()] = Sensor(message[3].strip(), message[4].strip(), message[5].strip(), message[6].strip())
+                        node_addresses[client] = str(message[3]).strip()
+                        update_neighbors(nodes)
+                        update_distance(nodes)
 
-        #Control Server STDIN commands
-        '''
-        inp = input().split()
-        if inp[0].upper() == "SENDDATA" and len(inp) == 3:
-            pass
-        elif inp[0].upper() == "QUIT" and len(inp) == 1:
-            quit(client_socket)
-            return
-        else:
-            print("Invalid arguments")
-        '''
-
-        while True:
-            #read_ready, _, _ = select.select([client_socket],[],[])
-            message = client_socket.recv(1024)
-            if message:
-                message = str(message.decode('utf-8'))
-                print(message)
-                if message.find('hw4_client.py'):
-                    message = message.replace('[','').replace(']','').replace('\'','').split(',')
-                    nodes[str(message[3]).strip()] = Sensor(message[3].strip(), message[4].strip(), message[5].strip(), message[6].strip())
+                        print("\nUpdated neighbors and distance")
+                        for id in nodes:
+                            print(nodes[id])
+                        print()
+                    else:
+                        #Commands received from Sensor client
+                        message = message.split()
+                        if len(message) == 2 and message[0].upper() == "WHERE":
+                            pass
+                        elif len(message) == 5 and message[0].upper() == "UPDATEPOSITION":
+                            #update positions of clients and update their distances and repopulate neighbors
+                            update_neighbors(nodes)
+                            update_distance(nodes)
+                        elif len(message) == 2 and message[0].upper() == "DATAMESSAGE":
+                            pass
+                        else:
+                            print("Invalid arguments from client")
+                else:
+                    if node_addresses[client] in nodes:
+                        del nodes[node_addresses[client]]
+                    inputs.remove(client)
+                    remove_disconnected(nodes)
                     update_neighbors(nodes)
                     update_distance(nodes)
+                    client.close()
 
-                    print("\nUpdated neighbors and distance")
+                    print("Client {} has disconnected".format(node_addresses[client]))
+                    print("Updated neighbors and distance")
                     for id in nodes:
                         print(nodes[id])
                     print()
-                else:
-                    #Commands received from Sensor client
-                    message = message.split()
-                    if message[0].upper() == "WHERE":
-                        pass
-                    elif message[0].upper() == "UPDATEPOSITION":
-                        #update positions of clients and update their distances and repopulate neighbors
-                        update_neighbors(nodes)
-                        update_distance(nodes)
-                    else:
-                        print("Invalid arguments from client")
-            else:
-                break
+                    continue
+
+
+
+
+
 
 
 if __name__ == '__main__':
