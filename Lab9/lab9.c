dc069aab8488df7b96e54f98cf5004650981bd52
Dani <sreil926@gmail.com>
2020-12-07 17:53:02 -0500

    modified lab9

--- Lab9/lab9.c
+++ Lab9/lab9.c
@@ -9,34 +9,40 @@
 #include <arpa/inet.h>
 
 int main(int argc, char* argv[]) {
+   int option_value[1];
+   socklen_t option_len = sizeof(option_value);
+   int sock;
+   struct sockaddr_in servaddr;
+
+
    if (argc != 2) {
       printf("Proper usage is %s [IP Hostname]\n", argv[0]);
       return EXIT_FAILURE;
    }
-
-   struct sockaddr_in servaddr;
+   
    bzero(&servaddr,sizeof(servaddr));
    servaddr.sin_family = AF_INET;
    servaddr.sin_port = htons(80);
    inet_aton(argv[1], &servaddr.sin_addr);
 
-   int option_value;
-   socklen_t option_len; 
+   sock = socket(AF_INET, SOCK_STREAM, 0);
 
-   int sock = socket(AF_INET, SOCK_STREAM, 0);
+   getsockopt(sock, IPPROTO_TCP, TCP_MAXSEG, option_value, &option_len);
+   printf("Prior values:\nMMS: %d\n", *option_value);
 
-   getsockopt(sock, IPPROTO_TCP, TCP_MAXSEG, &option_value, &option_len);
-   printf("Prior values:\nMMS: %d\n", option_value);
-   getsockopt(sock, IPPROTO_TCP, SO_RCVBUF, &option_value, &option_len);
-   printf("RCVBUF: %d\n\n", option_value);
-
+   option_len = sizeof(option_value);
+   getsockopt(sock, IPPROTO_TCP, SO_RCVBUF, option_value, &option_len);
+   printf("RCVBUF: %d\n\n", *option_value);
 
    connect(sock, (struct sockaddr*)&servaddr, sizeof(servaddr));
 
-   getsockopt(sock, IPPROTO_TCP, TCP_MAXSEG, &option_value, &option_len);
-   printf("After connection\nMMS: %d\n", option_value);
-   getsockopt(sock, IPPROTO_TCP, SO_RCVBUF, &option_value, &option_len);
-   printf("RCVBUF: %d\n", option_value);
+   option_len = sizeof(option_value);
+   getsockopt(sock, IPPROTO_TCP, TCP_MAXSEG, option_value, &option_len);
+   printf("After connection\nMMS: %d\n", *option_value);
+
+   option_len = sizeof(option_value);
+   getsockopt(sock, IPPROTO_TCP, SO_RCVBUF, option_value, &option_len);
+   printf("RCVBUF: %d\n", *option_value);
 
    close(sock);
 
